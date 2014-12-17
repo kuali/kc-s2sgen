5808f3e1a7dacce04857076d432cfc90a7cd9426
anithapolus <anitha@polussoftware.com>
2014-12-17 18:52:40 +0530

    KCSUPPORT-601  Other Degree Information not printing on grants.gov form which should retrieve from the PHS398 Fellowshop Supplimental V2 questionnaire

--- coeus-s2sgen-impl/src/main/java/org/kuali/coeus/s2sgen/impl/generate/support/PHS398FellowshipSupplementalV2_0Generator.java
+++ coeus-s2sgen-impl/src/main/java/org/kuali/coeus/s2sgen/impl/generate/support/PHS398FellowshipSupplementalV2_0Generator.java
@@ -247,6 +247,15 @@ public class PHS398FellowshipSupplementalV2_0Generator extends PHS398FellowshipS
                             graduateDegreeSought.setDegreeType(DegreeTypeDataType.MDOT_OTHER_DOCTOR_OF_MEDICINE);
                             graduateDegreeSought.setOtherDegreeTypeText(answer);
                             break;
+                        case OTHER_DBOTH:
+                       	   if (graduateDegreeSought.getDegreeType().equals(DegreeTypeDataType.OTH_OTHER)) {                    		  
+                              	   graduateDegreeSought.setOtherDegreeTypeText(answer);                    		   
+                       	   }                        	
+                           	break;
+                         case OTHER_DOCT:
+                           	graduateDegreeSought.setDegreeType(DegreeTypeDataType.DOTH_OTHER_DOCTORATE);
+                           	graduateDegreeSought.setOtherDegreeTypeText(answer);
+                           	break;
                         case BROAD_TRAINING:
                         case FIELD_TRAINING:
                             if (!answer.toUpperCase().equals("SUB CATEGORY NOT FOUND"))
