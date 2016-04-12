1903e91a2ff444cfe0b0322e7a83840788e52331
Travis Schneeberger <leo.herbie@gmail.com>
2016-04-12 12:24:11 -0400

    RESKC-933: add support for: S2S Form D: PHS 398 Career Development Award Supplemental Form 3.0 update for 2016

--- coeus-s2sgen-impl/src/main/java/org/kuali/coeus/s2sgen/impl/generate/support/PHS398CareerDevelopmentAwardSupBaseGenerator.java
+++ coeus-s2sgen-impl/src/main/java/org/kuali/coeus/s2sgen/impl/generate/support/PHS398CareerDevelopmentAwardSupBaseGenerator.java
@@ -23,40 +23,35 @@ import org.kuali.coeus.s2sgen.impl.person.S2SProposalPersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
-/**
- * This abstract class has methods that are common to all the versions of
- * PHS398CareerDevelopmentAwardSup form.
- * 
- * @author Kuali Research Administration Team (kualidev@oncourse.iu.edu)
- */
 public abstract class PHS398CareerDevelopmentAwardSupBaseGenerator extends S2SBaseFormGenerator {
 
-	public static final int NARRATIVE_TYPE_INTRODUCTION_TO_APPLICATION = 70;
-	public static final int NARRATIVE_TYPE_SPECIFIC_AIMS = 71;
-	public static final int NARRATIVE_TYPE_BACKGROUND_SIGNIFICANCE = 72;
-	public static final int NARRATIVE_TYPE_RESEARCH_DESIGN_METHODS = 73;
-	public static final int NARRATIVE_TYPE_INCLUSION_ENROLLMENT_REPORT = 79;
-	public static final int NARRATIVE_TYPE_PROGRESS_REPORT_PUBLICATION_LIST = 80;
-	public static final int NARRATIVE_TYPE_PROTECTION_OF_HUMAN_SUBJECTS = 74;
-	public static final int NARRATIVE_TYPE_INCLUSION_OF_WOMEN_AND_MINORITIES = 75;
-	public static final int NARRATIVE_TYPE_TARGETED_PLANNED_ENROLLMENT_TABLE = 76;
-	public static final int NARRATIVE_TYPE_INCLUSION_OF_CHILDREN = 77;
-	public static final int NARRATIVE_TYPE_VERTEBRATE_ANIMALS = 78;
-	public static final int NARRATIVE_TYPE_SELECT_AGENT_RESEARCH = 81;
-	public static final int NARRATIVE_TYPE_PHS_CAREER_PRELIM_STUDIES_PROGREP = 82;
-	public static final int NARRATIVE_TYPE_PHS_CAREER_CONSORTIUM_CONTRACT = 83;
-	public static final int NARRATIVE_TYPE_PHS_CAREER_RESOURCE_SHARING_PLAN = 84;
-	public static final int NARRATIVE_TYPE_CANDIDATE_BACKGROUND = 62;
-	public static final int NARRATIVE_TYPE_CAREER_GOALS_AND_OBJECTIVES = 63;
-	public static final int NARRATIVE_TYPE_CAREER_DEVELOPMENT_AND_TRAINING = 64;
-	public static final int NARRATIVE_TYPE_RESPONSIBLE_CONDUCT_OF_RESEARCH = 65;
-	public static final int NARRATIVE_TYPE_PHS398_MENTORING_PLAN = 66;
-	public static final int NARRATIVE_TYPE_PHS398_MENTOR_STATEMENTS_LETTERS = 67;
-	public static final int NARRATIVE_TYPE_PSH398_INSTITUTIONAL_ENVIRONMENT = 68;
-	public static final int NARRATIVE_TYPE_PHS398_INSTITUTIONAL_COMMITMENT = 69;
-	public static final int NARRATIVE_TYPE_PHS_CAREER_APPENDIX = 85;
-	public static final int NARRATIVE_TYPE_PHS_CAREER_REASEARCH_STRATEGY = 128;
-
+	protected static final int NARRATIVE_TYPE_INTRODUCTION_TO_APPLICATION = 70;
+	protected static final int NARRATIVE_TYPE_SPECIFIC_AIMS = 71;
+	protected static final int NARRATIVE_TYPE_BACKGROUND_SIGNIFICANCE = 72;
+	protected static final int NARRATIVE_TYPE_RESEARCH_DESIGN_METHODS = 73;
+	protected static final int NARRATIVE_TYPE_INCLUSION_ENROLLMENT_REPORT = 79;
+	protected static final int NARRATIVE_TYPE_PROGRESS_REPORT_PUBLICATION_LIST = 80;
+	protected static final int NARRATIVE_TYPE_PROTECTION_OF_HUMAN_SUBJECTS = 74;
+	protected static final int NARRATIVE_TYPE_INCLUSION_OF_WOMEN_AND_MINORITIES = 75;
+	protected static final int NARRATIVE_TYPE_TARGETED_PLANNED_ENROLLMENT_TABLE = 76;
+	protected static final int NARRATIVE_TYPE_INCLUSION_OF_CHILDREN = 77;
+	protected static final int NARRATIVE_TYPE_VERTEBRATE_ANIMALS = 78;
+	protected static final int NARRATIVE_TYPE_SELECT_AGENT_RESEARCH = 81;
+	protected static final int NARRATIVE_TYPE_PHS_CAREER_PRELIM_STUDIES_PROGREP = 82;
+	protected static final int NARRATIVE_TYPE_PHS_CAREER_CONSORTIUM_CONTRACT = 83;
+	protected static final int NARRATIVE_TYPE_PHS_CAREER_RESOURCE_SHARING_PLAN = 84;
+	protected static final int NARRATIVE_TYPE_CANDIDATE_BACKGROUND = 62;
+	protected static final int NARRATIVE_TYPE_CAREER_GOALS_AND_OBJECTIVES = 63;
+	protected static final int NARRATIVE_TYPE_CAREER_DEVELOPMENT_AND_TRAINING = 64;
+	protected static final int NARRATIVE_TYPE_RESPONSIBLE_CONDUCT_OF_RESEARCH = 65;
+	protected static final int NARRATIVE_TYPE_PHS398_MENTORING_PLAN = 66;
+	protected static final int NARRATIVE_TYPE_PHS398_MENTOR_STATEMENTS_LETTERS = 67;
+	protected static final int NARRATIVE_TYPE_PSH398_INSTITUTIONAL_ENVIRONMENT = 68;
+	protected static final int NARRATIVE_TYPE_PHS398_INSTITUTIONAL_COMMITMENT = 69;
+	protected static final int NARRATIVE_TYPE_PHS_CAREER_APPENDIX = 85;
+	protected static final int NARRATIVE_TYPE_PHS_CAREER_REASEARCH_STRATEGY = 128;
+	protected static final int NARRATIVE_TYPE_PHS_CAREER_SUPPPORT_LTRS = 144;
+	
     @Autowired
     @Qualifier("s2SProposalPersonService")
     protected S2SProposalPersonService s2SProposalPersonService;
